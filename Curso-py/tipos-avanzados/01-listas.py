134239bd6d470954841030a253844c219950c4ba
Pablo Bossio <pablico604090@gmail.com>
2024-05-07 13:46:39 -0300

    Modify content

--- Curso-py/tipos-avanzados/01-listas.py
+++ Curso-py/tipos-avanzados/01-listas.py
@@ -1,12 +1,42 @@
-numeros = [1, 2, 3]
-letras = ["a", "b", "c"]
-palabras = ["canchito", "feliz"]
-palabrasFelices = ["chanchito", "feliz", "Felipe", "alumno"]
-booleans = [True, False, True, True]
-matriz = [[0, 1], [1, 0]]
-ceros = [0] * 10
-alfanumerico = numeros + letras
-rango = list(range(1, 11))
-chars = list("hola mundo")
+# numeros = [1, 2, 3]
+# letras = ["a", "b", "c"]
+# palabras = ["canchito", "feliz"]
+# palabrasFelices = ["chanchito", "feliz", "Felipe", "alumno"]
+# booleans = [True, False, True, True]
+# matriz = [[0, 1], [1, 0]]
+# ceros = [0] * 10
+# alfanumerico = numeros + letras
+# rango = list(range(1, 11))
+# chars = list("hola mundo")
 
-print(rango)
+# print(rango)
+
+numbers = [10, 5, 7, 2, 1]
+# Imprimiendo contenido de la lista original.
+print("Contenido de la lista original:", numbers)
+
+numbers[0] = 111
+# Imprimiendo contenido de la lista anterior.
+print("\nPrevio contenido de la lista:", numbers)
+
+# Copiando el valor del quinto elemento al segundo elemento.
+numbers[1] = numbers[4]
+# Imprimiendo el contenido de la lista actual.
+print("Nuevo contenido de la lista:", numbers)
+print(numbers[0])  # Accediendo al primer elemento de la lista.
+# Imprimiendo la longitud de la lista.
+print("\nLongitud de la lista:", len(numbers))
+
+# my_list = []  # Creando una lista vacía.
+
+# for i in range(5): rellnar lista con for.
+#     my_list.append(i + 1)
+
+# print(my_list)
+
+# my_list = []  # Creando una lista vacía.
+
+# for i in range(5): rellenado lista con for, haciendo que vaya de mayor a menor.
+#     my_list.insert(0, i + 1)
+
+# print(my_list)
